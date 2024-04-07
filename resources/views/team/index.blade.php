5f8d307f0fff0c6b617f3bf44c4b3077bf21d938
USER-2020 <hackingetico2021@gmail.com>
2024-04-07 06:28:57 -0500

    change class buton

--- resources/views/team/index.blade.php
+++ resources/views/team/index.blade.php
@@ -11,7 +11,7 @@
             <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
                 <div class="w-full">
                     @include('team.partials.list-teams', ['teams' => $teams])
-                    <x-primary-button><a href="{{ route('index.teams') }}" class="inline-block bg-blue-500 text-white px-4 py-2 rounded-md mt-4">Add New Team</a></x-primary-button>
+                    <x-primary-button><a href="{{ route('index.teams') }}" >Add New Team</a></x-primary-button>
                 </div>
             </div>
 
